0af9368918f365f4666105c1593013c4f11b74f5
SERHIHUB <chokorik@gmail.com>
2023-11-13 23:21:25 +0200

    mayje

---
+++ js/task-3.js
@@ -0,0 +1,35 @@
+"use strict";
+
+class StringBuilder {
+  #value;
+
+  constructor(initialValue) {
+    this.#value = initialValue;
+  }
+
+  getValue() {
+    return this.#value;
+  }
+
+  padEnd(str) {
+    this.#value.push(str);
+  }
+
+  padStart(str) {
+    this.#value.unshift(str);
+  }
+
+  //   padBoth(str) {
+  //     this.#value.push(str);
+  //     this.#value.unshift(str);
+  //   }
+}
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
